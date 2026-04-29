a097d18067509914d4cc7643579a20e7532d09bd
Aditya kumar Choubey <choubeyaditya85@gmail.com>
2026-04-29 19:02:08 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -392,7 +392,7 @@ if row.get('emp_screenshot'):
                             file_name=file_name,
                             key=f"admin_dl_{uuid.uuid4().hex[:5]}"
                         )
-     else:
+else:
     st.info("No proofs uploaded yet.")
                         
     st.divider()
